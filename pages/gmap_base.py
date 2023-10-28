270abce6512435121c8d975828611bcc30fa50a5
TimV11 <t@4vielhauer.de>
2023-10-28 11:13:57 +0200

    center data + import sentinel

--- pages/gmap_base.py
+++ pages/gmap_base.py
@@ -3,8 +3,32 @@ import ee
 import streamlit as st
 import geemap.foliumap as geemap
 
-st.set_page_config(layout="wide")
+def mask_s2_clouds(image):
+  """Masks clouds in a Sentinel-2 image using the QA band.
 
+  Args:
+      image (ee.Image): A Sentinel-2 image.
+
+  Returns:
+      ee.Image: A cloud-masked Sentinel-2 image.
+  """
+  qa = image.select('QA60')
+
+  # Bits 10 and 11 are clouds and cirrus, respectively.
+  cloud_bit_mask = 1 << 10
+  cirrus_bit_mask = 1 << 11
+
+  # Both flags should be set to zero, indicating clear conditions.
+  mask = (
+      qa.bitwiseAnd(cloud_bit_mask)
+      .eq(0)
+      .And(qa.bitwiseAnd(cirrus_bit_mask).eq(0))
+  )
+
+  return image.updateMask(mask).divide(10000)
+
+
+st.set_page_config(layout="wide")
 
 st.title("NeyJourNeyJour")
 
@@ -16,41 +40,51 @@ Map.add_basemap("ESA WorldCover 2020 S2 TCC")
 Map.add_basemap("HYBRID")
 
 
-"test"
-esa = ee.ImageCollection("ESA/WorldCover/v100").first()
-esa_vis = {"bands": ["Map"]}
-esri = ee.ImageCollection(
-    "projects/sat-io/open-datasets/landcover/ESRI_Global-LULC_10m"
-).mosaic()
-esri_vis = {
-    "min": 1,
-    "max": 10,
-    "palette": [
-        "#1A5BAB",
-        "#358221",
-        "#A7D282",
-        "#87D19E",
-        "#FFDB5C",
-        "#EECFA8",
-        "#ED022A",
-        "#EDE9E4",
-        "#F2FAFF",
-        "#C8C8C8",
-    ],
+esa = ee.ImageCollection("COPERNICUS/S2_SR_HARMONIZED").first()
+esa_vis = {"bands": ["B2","B3","B4","B8"]}
+#esri = ee.ImageCollection(
+#    "projects/sat-io/open-datasets/landcover/ESRI_Global-LULC_10m"
+#).mosaic()
+#esri_vis = {
+#    "min": 1,
+#    "max": 10,
+#    "palette": [
+#        "#1A5BAB",
+#        "#358221",
+#        "#A7D282",
+#        "#87D19E",
+#        "#FFDB5C",
+#        "#EECFA8",
+#        "#ED022A",
+#        "#EDE9E4",
+#        "#F2FAFF",
+#        "#C8C8C8",
+#    ],
+#}
+
+dataset = (
+    ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+    .filterDate('2020-01-01', '2022-01-30')
+    # Pre-filter to get less cloudy granules.
+    .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 20))
+    .map(mask_s2_clouds)
+)
+
+visualization = {
+    'min': 0.0,
+    'max': 0.3,
+    'bands': ['B4'],
 }
 
-
-markdown = """
-    - [Dynamic World Land Cover](https://developers.google.com/earth-engine/datasets/catalog/GOOGLE_DYNAMICWORLD_V1?hl=en)
-    - [ESA Global Land Cover](https://developers.google.com/earth-engine/datasets/catalog/ESA_WorldCover_v100)
-    - [ESRI Global Land Cover](https://samapriya.github.io/awesome-gee-community-datasets/projects/esrilc2020)
-
-"""
+#m = geemap.Map()
+#m.set_center(83.277, 17.7009, 12)
+#m.add_layer(dataset.mean(), visualization, 'RGB')
+#   m
 
 with col2:
 
-    longitude = st.number_input("Longitude", -180.0, 180.0, -89.3998)
-    latitude = st.number_input("Latitude", -90.0, 90.0, 43.0886)
+    longitude = st.number_input("Longitude", -180.0, 180.0, 8.559718)
+    latitude = st.number_input("Latitude", -90.0, 90.0, 49.794677)
     zoom = st.number_input("Zoom", 0, 20, 11)
 
     Map.setCenter(longitude, latitude, zoom)
@@ -66,32 +100,29 @@ with col2:
 
     layers = {
         "Dynamic World": geemap.ee_tile_layer(dw, {}, "Dynamic World Land Cover"),
-        "ESA Land Cover": geemap.ee_tile_layer(esa, esa_vis, "ESA Land Cover"),
-        "ESRI Land Cover": geemap.ee_tile_layer(esri, esri_vis, "ESRI Land Cover"),
+        #"ESA Land Cover": geemap.ee_tile_layer(esa, esa_vis, "ESA Land Cover"),
+        #"ESRI Land Cover": geemap.ee_tile_layer(esri, esri_vis, "ESRI Land Cover"),
     }
 
-    options = list(layers.keys())
-    left = st.selectbox("Select a left layer", options, index=1)
-    right = st.selectbox("Select a right layer", options, index=0)
+    #options = list(layers.keys())
+    #left = st.selectbox("Select a left layer", options, index=1)
+    #right = st.selectbox("Select a right layer", options, index=0)
 
-    left_layer = layers[left]
-    right_layer = layers[right]
+    #left_layer = layers[left]
+    #right_layer = layers[right]
 
-    Map.split_map(left_layer, right_layer)
+    #Map.split_map(left_layer, right_layer)
 
-    legend = st.selectbox("Select a legend", options, index=options.index(right))
-    if legend == "Dynamic World":
-        Map.add_legend(
-            title="Dynamic World Land Cover",
-            builtin_legend="Dynamic_World",
-        )
-    elif legend == "ESA Land Cover":
-        Map.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
-    elif legend == "ESRI Land Cover":
-        Map.add_legend(title="ESRI Land Cover", builtin_legend="ESRI_LandCover")
-
-    with st.expander("Data sources"):
-        st.markdown(markdown)
+    #legend = st.selectbox("Select a legend", options, index=options.index(right))
+    #if legend == "Dynamic World":
+        #Map.add_legend(
+            #title="Dynamic World Land Cover",
+            #builtin_legend="Dynamic_World",
+        #)
+    #elif legend == "ESA Land Cover":
+        #Map.add_legend(title="ESA Land Cover", builtin_legend="ESA_WorldCover")
+    #elif legend == "ESRI Land Cover":
+        #Map.add_legend(title="ESRI Land Cover", builtin_legend="ESRI_LandCover")
 
 
 with col1:
